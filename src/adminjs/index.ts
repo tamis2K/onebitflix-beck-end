3c1d147b680882a964645f6422a8201334471ec0
Mateus Varnier <mateuschioquetta@gmail.com>
2023-11-06 19:46:57 -0300

    Projeto

---
+++ src/adminjs/index.ts
@@ -0,0 +1,30 @@
+import AdminJs from "adminjs";
+import AdminJsExpress from "@adminjs/express";
+import AdminJsSequelize from "@adminjs/sequelize";
+import { sequelize } from "../database";
+import { adminJsResources } from "./resources";
+import { locale } from "./locale";
+import { dashboardOptions } from "./dashboard";
+import { brandingOptions } from "./branding";
+import { authtenticationOptions } from "./authentication";
+
+AdminJs.registerAdapter(AdminJsSequelize);
+
+export const adminJs = new AdminJs({
+  databases: [sequelize],
+  rootPath: "/admin",
+  resources: adminJsResources,
+  locale: locale,
+  branding: brandingOptions,
+  dashboard: dashboardOptions,
+});
+
+export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(
+  adminJs,
+  authtenticationOptions,
+  null,
+  {
+    resave: false,
+    saveUninitialized: false,
+  }
+);
